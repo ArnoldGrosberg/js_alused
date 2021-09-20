28384ecfab30d94d0cb42c35252bd37de1e47c83
Arnold Grosberg <arnold.grosberg@khk.ee>
2021-09-20 11:26:20 +0300

    JS variables

---
+++ js/Muutujad.js
@@ -0,0 +1,32 @@
+// Muutujad
+// var, let, const
+
+//let name = "Anna Karutina";
+// kasutamine
+// console.log(name);
+// name = "Mihhail Karutin"
+// console.log(name);
+
+// lubatud märgid nime loomisel - tähed, numbrid, _, $,
+
+// let $ = "dollar";
+// console.log($);
+
+const name = "Anna Karutina";
+console.log(name);
+// const ei saa muuta väärtust
+// name = "Mihhail Karutin";
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.sort(6);
+console.log(numbers);
+
+// objektid
+const person = {
+	name: "Kadi"
+	ago: 20
+
+}
+person.name = "Mati";
+person.age = 15;
+console.log(person);
