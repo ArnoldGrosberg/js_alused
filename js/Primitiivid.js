4c5f7970555af79a2919371e6061a4cba14190c1
Arnold Grosberg <arnold.grosberg@khk.ee>
2021-09-20 11:44:36 +0300

    alused

---
+++ js/Primitiivid.js
@@ -0,0 +1,48 @@
+// andmetüübid
+
+// PRimitiivid
+// String
+const name = "Anna";
+console.log(typeof name);
+
+// Numbrid
+const age = 15;
+console.log(typeof age);
+
+
+const persent = 56.3;
+console.log(typeof persent);
+
+
+// Boolean true või false
+const hasKIds = true;
+console.log(typeof hasKIds);
+
+// NUll
+const airplane = null;
+console.log(typeof airplane);
+
+// Undefined
+let test;
+console.log(typeof test);
+
+// Symbol
+const sym = Symbol();
+console.log(sym);
+
+
+// Objects - reference types -tüübid, mille sisu saab viite kaudu
+const hobbies = ["movies, music"];
+console.log(typeof hobbies);
+
+// Object
+const person = {
+	name: "Kadi"
+	age: 15
+}
+console.log(typeof person);
+
+// näiteks kuupäeva object
+const today = new Date();
+console.log(typeof today);
+console.log(today);
