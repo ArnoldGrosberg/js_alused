10d3f4a2ff7ba72bd2adb5215bc96cbe0e014a82
Arnold Grosberg <arnold.grosberg@khk.ee>
2021-09-27 11:26:27 +0300

    Tingimuslaused - condition statement

--- app.js
+++ app.js
@@ -1,26 +1,81 @@
-let val;
+/*
+if(tingimus) {     if puhul
+	// tegevused if puhul
+} else {
+	// tegevused else puhul
+}	
+*/
+const id = 100;
 
-const today = new Date();
+// == võrdub
+// != Ei võrdu
+// === võrdub ja tüǘbikontroll
+// !== Ei võrdu ja tüübikontroll
 
-val = today.getMonth(); // 0 on jaanuar
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime(); // Millisekundid alatest jaanuarist GetTime
 
-let birthday = new Date('9/27/2001');
-birthday = new Date('September 27 2001');
+let testId;
 
-birthday.setMonth(2); // 0 - jaanuar
-birthday.setDate(12);
-birthday.setFullYear(2001);
-birthday.setHours(11);
-birthday.setMinutes(25);
-birthday.setSeconds(50);
+console.log(testId);
 
-console.log(birthday);
-console.log(val);
+/*
+if(typeof testId !== undefined){
+	console.log(`id on ${testId}`);
+} else {
+	console.log("Vale");
+}
+*/
+
+
+/*
+// > Ja <
+if(id >100){
+	console.log(`id on ${id} - suurem kui 100`)
+} else{
+	console.log(`id on ${id} - väiksem või võrdne 100`)
+}
+*/
+
+
+/*
+// if - else if - else
+const color = "blue";
+
+if(color=="red"){
+	console.log("Stop!");
+} else if (color == "yellow"){
+	console.log("Attention!");
+} else if (color == "green"){
+console.log("Go!");
+} else {
+	console.log('Color is not accepted!');
+}
+*/
+
+// and - &&
+const name = "Kadi";
+const age = 70;
+
+if(age >0 && age <= 12){
+	console.log(`${name} on laps`);
+} else if (age > 12 && age < 18){
+	console.log(`${name} on nooruk`);
+} else {
+	console.log(`${name} on täiskasvanud`);
+}
+
+
+// or = ||
+if(age < 18 || age > 65){
+	console.log(`${name} ei saa registreerida`)
+} else {
+	console.log(`${name} on registreeritud`);
+}
+
+// ? - ternary operaator
+console.log(id === 100 ? 'Õige' : 'vale');
+
+// ilma ()
+if(id === 100) 
+	console.log('Korras');
+else 
+	console.log("Ei ole korras");
