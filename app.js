8c4302c3e23d6802adf3339ef349f81298ffbba4
Arnold Grosberg <arnold.grosberg@khk.ee>
2021-09-28 11:21:26 +0300

    DOM

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
+let val;
 
-function tere(){
-	let i = document.getElementById("hello").value;
-	document.getElementById("ul").innerHTML += `<li>${i}`;
-	
-}
+val = document;
+val = document.all;
+val = document.all[5];
+val = document.all.length;
+
+val = document.head;
+val = document.body;
+
+val = document.doctype;
+
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.froms;
+val = document.forms[0];
+val = document.forms[0].method;
+val = document.forms[0].action;
+val = document.forms[0].id;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList;
+
+
+val = document.scrips;
+val = document.scripts[0];
+val = document.scripts[1];
+val = document.scripts[0].getAttribute('src');
+
+console.log(val);
