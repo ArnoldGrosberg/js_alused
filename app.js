5b9be6846800ab788ddcbe2f915066be5d8bb512
Arnold Grosberg <arnold.grosberg@khk.ee>
2021-10-13 10:38:43 +0300

    tried making localstorage

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 // event elements
 const taskList = document.querySelector('ul');
 const delAllBtn = document.getElementById('#del-tasks');
+const form = document.querySelector('form');
+const taskInput = document.querySelector('#task');
+// submit 
+form.addEventListener('submit', addToList)
+function addToList(e) {
+  const li = document.createElement('li');
+  const list = document.querySelector('ul');
+
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  const link = document.createElement('a');
+  link.className = 'secondary-content'
+  link.appendChild(document.createTextNode('X'));
+  link.setAttribute('href', '#')
+
+  console.log(taskList.innerHTML.element);
+  localStorage.setItem(taskList.innerHTML);
+
+
+  li.appendChild(link);
+  list.appendChild(li);
+
+  taskInput.value = '';
+  e.preventDefault();
+}
 
 // click element kustutamiseks
 taskList.addEventListener('click', deleteTask);
@@ -11,6 +37,8 @@ function deleteTask(e) {
 if(e.target.textContent = 'X'){
     if(confirm('Are you sure to delete this task?')){
       e.target.parentElement.remove();
+      localStorage.removeItem(e.target.parentElement);
+
     }
   }
 }
@@ -20,4 +48,6 @@ function deleteTasks(e) {
 while(taskList.firstChild){
     taskList.removeChild(taskList.firstChild);
   }
+  localStorage.clear();
 }
+
